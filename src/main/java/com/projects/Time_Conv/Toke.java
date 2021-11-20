23bd5b0171e641367929c80dbddcd62b0a035b2c
Joel-Marc <joel.marceline3@gmail.com>
2021-11-20 20:37:57 +0530

    hmm

--- src/main/java/com/projects/Time_Conv/Toke.java
+++ src/main/java/com/projects/Time_Conv/Toke.java
@@ -2,7 +2,11 @@ package com.projects.Time_Conv;
 
 import java.io.IOException;
 import java.util.Properties;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
@@ -37,6 +41,13 @@ public class Toke {
         // HttpResponse<String> httpResponse = client.send(request,
         // HttpResponse.BodyHandlers.ofString());
         // System.out.println(httpResponse.body());
+
+        SparkSession spark = SparkSession.builder().appName("Java Spark SQL Example").getOrCreate();
+
+        StructType schema = new StructType().add("Abr", "string").add("Zone", "string").add("Dilate", "string");
+
+        Dataset<Row> df = spark.read().option("mode", "DROPMALFORMED").schema(schema)
+                .csv("/home/jmarc/Desktop/Time_Conv/src/main/java/com/projects/Time_Conv/timezones.csv");
 
     }
 }
