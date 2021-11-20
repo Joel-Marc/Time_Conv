629183bb11792285a7e670a6e088d43c477d32a4
Mithilesh-V <cv.mithilesh@gmail.com>
2021-11-20 22:52:09 +0530

    add

---
+++ src/main/java/com/projects/Time_Conv/controller.java
@@ -0,0 +1,24 @@
+package com.projects.Time_Conv;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class controller 
+{	
+	@RequestMapping("/")
+	public String index()
+	{
+		return "index";
+	}
+	
+	@RequestMapping(value="/Savesearch" , method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute search Search)
+	{
+		System.out.println(Search);
+	}
+	
+
+}
---
+++ src/main/java/com/projects/Time_Conv/search.java
@@ -0,0 +1,22 @@
+package com.projects.Time_Conv;
+
+public class search {
+	
+	private String search;
+
+	public String getSearch() {
+		return search;
+	}
+
+	public void setSearch(String search) {
+		this.search = search;
+	}
+
+	@Override
+	public String toString() {
+		return "search [search=" + search + "]";
+	}
+	
+	
+
+}
