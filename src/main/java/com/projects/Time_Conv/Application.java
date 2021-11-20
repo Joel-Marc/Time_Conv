c87b37a12dbcbde8504d1e50982ef36d3493859c
Mithilesh-V <cv.mithilesh@gmail.com>
2021-11-20 23:02:17 +0530

    add3

---
+++ src/main/java/com/projects/Time_Conv/Application.java
@@ -0,0 +1,10 @@
+package com.projects.Time_Conv;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
+}
