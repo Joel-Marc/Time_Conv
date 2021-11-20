85f5d5f50991ba48aabb799b5c762f558b369882
Joel-Marc <joel.marceline3@gmail.com>
2021-11-20 22:55:38 +0530

    hmm

--- src/main/java/com/projects/Time_Conv/controller.java
+++ src/main/java/com/projects/Time_Conv/controller.java
@@ -1,24 +1,21 @@
 package com.projects.Time_Conv;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class controller 
-{	
+public class controller {
 	@RequestMapping("/")
-	public String index()
-	{
+	public String index() {
 		return "index";
 	}
-	
-	@RequestMapping(value="/Savesearch" , method = RequestMethod.POST)
-	public ModelAndView save(@ModelAttribute search Search)
-	{
+
+	@RequestMapping(value = "/Savesearch", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute search Search) {
 		System.out.println(Search);
 	}
-	
 
 }
